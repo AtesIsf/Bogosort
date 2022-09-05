80c8c9fcb4745a36e5cd71873070d0cee94642e0
AtesIsf <isfendiyar510@gmail.com>
2022-09-05 19:00:43 +0300

    Before first test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+from datetime import datetime
+import random
 
 def is_sorted(arr):
     for i in range(len(arr)-1):
@@ -7,9 +9,16 @@ def is_sorted(arr):
     return True
 
 def bogosort(arr):
-    pass
+    while (is_sorted(arr) == False):
+        random.shuffle(arr)
 
 if __name__ == "__main__":
     arr = (3, 4, 1, 5, 2)
-    arr2 = (1, 2, 3, 4, 5)
-    # bogosort(arr)
+
+    start = time.time()
+    bogosort(arr)
+    end = time.time()
+
+    with open("logs.txt", "a") as file:
+        curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        file.write(f"Date-time: {curr_datetime}, Elapsed time: {end-start}s\n")
